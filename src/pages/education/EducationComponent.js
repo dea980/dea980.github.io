6055f2f7218a2446da29d4d4b0d0d84eef7aaeaa
dan.kim <dan.kim@indyaudiolabs.com>
2025-01-14 14:28:46 +0900

    Remove certifications component and related imports

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -3,11 +3,9 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
 import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
 import { competitiveSites } from "../../portfolio";
-import { certifications } from "../../portfolio";
 import "./EducationComponent.css";
 import { Fade } from "react-reveal";
 
@@ -39,9 +37,6 @@ class Education extends Component {
             </div>
           </Fade>
           <Educations theme={this.props.theme} />
-          {certifications.certifications.length > 0 ? (
-            <Certifications theme={this.props.theme} />
-          ) : null}
         </div>
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
