76072e9e5346624b8ecc15b1989d1c18eb2dd92d
dan.kim <dan.kim@indyaudiolabs.com>
2025-01-14 15:13:33 +0900

    fix: move flex container styles to #root element to avoid conflicts with index.css

--- src/global.js
+++ src/global.js
@@ -8,14 +8,13 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    align-items: center;
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
+    transition: all 0.25s linear;
+  }
+
+  #root {
     display: flex;
     flex-direction: column;
-    margin: 0;
-    padding: 0;
     min-height: 100vh;
-    font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-    transition: all 0.25s linear;
   }`;
